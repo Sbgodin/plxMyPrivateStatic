1ad8fd7660be7a08f772bf10540abd5c45d25d99
Quent-in <pages_quentin@hotmail.com>
2016-04-20 19:51:24 +0200

    Occitan version

---
+++ lang/oc.php
@@ -0,0 +1,12 @@
+<?php
+$LANG = array(
+# admin: static.php
+'L_PROTECT'					=> 'Protegir la lectura de la pagina amb un senhal',
+'L_FORM_PROTECT'			=> 'Activar la proteccion',
+'L_FORM_PASSWORD'			=> 'Senhal',
+'L_FORM_PUBLIC_CONTENT'		=> 'Contengut public (visible solament se lo senhal es demandat)',
+# form
+'L_INVALID_PASSWORD'		=> 'Senhal invalide',
+'L_FORM_BTN_SEND'			=> 'Mandar',
+);
+?>
